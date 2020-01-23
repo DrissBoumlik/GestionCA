333283bf2d73994c730344959813273a04637be8
Driss Boumlik <boumlik.t@gmail.com>
2020-01-23 11:04:23 +0100

    Edit clickable cells styles

--- public/add_ons/stats/stats.js
+++ public/add_ons/stats/stats.js
@@ -744,11 +744,11 @@ $(function () {
                             let col = object.element_dt.cell(this).index().column + 1;
                             let row = object.element_dt.cell(this).index().row + 1;
                             let colText = $(tableId + " thead th:nth-child(" + col + ")").text();
-                            let rowText = $(tableId + " tbody tr:nth-child(" + row + ") td:first-child").text()
+                            let rowText = $(tableId + " tbody tr:nth-child(" + row + ") td:" + (params.details ? "nth-child(2)" : "first-child")).text();
                             if (object.columnName === 'Date_Heure_Note_Semaine') {
                                 colText = colText.split('_')[0];
                             }
-                            if (col !== 1) {
+                            if (col !== 1 || (params.details && col !== 2)) {
                                 window.location = APP_URL + '/all-stats?' +
                                     'row=' + object.rowName +
                                     '&rowValue=' + rowText +
