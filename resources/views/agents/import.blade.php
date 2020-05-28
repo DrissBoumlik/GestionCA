85a9d3a7de1594c98a966d4826db8b63ae14a9b0
Driss Boumlik <boumlik.t@gmail.com>
2020-05-28 16:18:37 +0000

    Fix absolute link

--- resources/views/agents/import.blade.php
+++ resources/views/agents/import.blade.php
@@ -65,7 +65,7 @@
                         </button>
                     </div>
                     <div class="btn-view-data-wrapper">
-                        <a href="/agents/all-data" class="btn btn-primary" target="_blank">
+                        <a href="agents/all-data" class="btn btn-primary" target="_blank">
                             <i class="fa fa-fw fa-view mr-1"></i> Visualiser les données importées
                         </a>
                     </div>
